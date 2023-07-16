07120f5fdb4b81588c2d9d0c1f7224ea36318e06
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-07-16 17:15:07 -0600

    New line fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 DAYS_IN_A_YEAR = 365
 HOURS_IN_A_DAY = 24
 MINUTES_IN_AN_HOUR = 60
-PROMPT = "Enter number of days you'd \nlike to translate into minutes \nor enter 'x' to exit: \n"
+PROMPT = "Enter number of days you'd \nlike to translate into minutes \nor enter 'x' to exit: \n\n"
 
 def days_to_minutes(days):
     return days * HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR
@@ -17,15 +17,15 @@ def main():
         try:
             days = int(user_input)
         except ValueError:
-            print("\nError: Please enter a numeric value")
+            print("\nError: Please enter a numeric value\n")
             continue
 
         if days <= 0:
-            print("\nError: Please enter a number greater than 0")
+            print("\nError: Please enter a number greater than 0\n")
         else:
             minutes = days_to_minutes(days)
             print("\n-----------------------------------------------")
             print(f"There are {minutes} minutes in {days} days")
-            print("-----------------------------------------------")
+            print("-----------------------------------------------\n")
 if __name__ == "__main__":
     main()
