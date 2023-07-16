5e7033291e3c709ca6f9dc66dd598a6ad8b5ce2d
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-07-16 11:17:00 -0600

    Using vars

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print(f"There are {365 * 24 * 60} minutes in a year")
+days_in_a_year = 365
+hours_in_a_day = 24
+minutes_in_an_hour = 60
+
+print(f"There are {days_in_a_year * hours_in_a_day * minutes_in_an_hour} minutes in a year")
