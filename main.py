67938ad9f48791109ddb3244bacaefb60df4db2a
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-07-16 17:10:55 -0600

    Moved to main()

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ PROMPT = "Enter number of days you'd \nlike to translate into minutes \nor enter
 def days_to_minutes(days):
     return days * HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR
 
-while True:
-    user_input = input(PROMPT)
+def main():
+    while True:
+        user_input = input(PROMPT)
 
-    if user_input == "x":
-        print("\nExiting...\nEnjoy your day!")
-        break
+        if user_input == "x":
+            print("\nExiting...\nEnjoy your day!")
+            break
 
-    try:
-        days = int(user_input)
-    except ValueError:
-        print("\nError: Please enter a numeric value")
-        continue
+        try:
+            days = int(user_input)
+        except ValueError:
+            print("\nError: Please enter a numeric value")
+            continue
 
-    if days <= 0:
-        print("\nError: Please enter a number greater than 0")
-    else:
-        minutes = days_to_minutes(days)
-        print("\n-----------------------------------------------")
-        print(f"There are {minutes} minutes in {days} days")
-        print("-----------------------------------------------")
+        if days <= 0:
+            print("\nError: Please enter a number greater than 0")
+        else:
+            minutes = days_to_minutes(days)
+            print("\n-----------------------------------------------")
+            print(f"There are {minutes} minutes in {days} days")
+            print("-----------------------------------------------")
+if __name__ == "__main__":
+    main()
