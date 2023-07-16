c5322d09bd9af7ed4e665140c38dee1e3156612c
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-07-16 14:06:13 -0600

    days_to_minutes function

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ days_in_a_year = 365
 hours_in_a_day = 24
 minutes_in_an_hour = 60
 
-print(f"There are {days_in_a_year * hours_in_a_day * minutes_in_an_hour} minutes in a year")
+def days_to_minutes(days):
+    return days * hours_in_a_day * minutes_in_an_hour
+
+user_input = input("Enter number of days you'd like to translate into minutes: \n")
+days = int(user_input)
+minutes = days_to_minutes(days)
+res = f"There are {minutes} minutes in {days} days"
+
+print(res)
