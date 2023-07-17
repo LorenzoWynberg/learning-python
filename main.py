ecc46f543e29cd34da56ad832f312c9178e890c5
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-07-16 20:31:02 -0600

    Refactored prompt and validation into get_valid_days_input()

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ PROMPT = "Enter number of days you'd \nlike to translate into minutes \nor enter
 def days_to_minutes(days):
     return days * HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR
 
-def main():
+def get_valid_days_input():
     while True:
         user_input = input(PROMPT)
 
         if user_input == "x":
-            print("\nExiting...\nEnjoy your day!")
-            break
+            return None
 
         try:
             days = int(user_input)
+            if days <= 0:
+                print("\nError: Please enter a number greater than 0\n")
+            else:
+                return days
         except ValueError:
             print("\nError: Please enter a numeric value\n")
-            continue
 
-        if days <= 0:
-            print("\nError: Please enter a number greater than 0\n")
+def main():
+    while True:
+        days = get_valid_days_input()
+        if days is None:
+            print("\nExiting...\nEnjoy your day!")
+            break
         else:
             minutes = days_to_minutes(days)
             print("\n-----------------------------------------------")
